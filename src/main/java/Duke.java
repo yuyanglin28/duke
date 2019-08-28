fcd769041f112e73226c651262565e53f73929ba
yuyanglin28 <annielin9908@gmail.com>
2019-08-28 18:08:20 +0800

    Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class Duke {
+    static Task[] taskList = new Task[100];
+    static int i = 0;
+
     public static void main(String[] args) {
 
-        Task[] taskList = new Task[100];
-        int i = 0;
         String line = "_______________________________________\n";
 
         System.out.print(line);
@@ -13,71 +14,92 @@ public class Duke {
         System.out.print(line);
 
         Scanner input = new Scanner(System.in);
-        while(true){
+        boolean valid;
+
+        while (true) {
             String command = input.next();
             System.out.print(line);
+            valid = true;
 
-            if (command.equals("list") ){
-                System.out.println("Here are the tasks in your list:");
-                for ( int j = 0; j<i; j++){
-                    System.out.println(j+1+"."+taskList[j]);
-                }
-            }else if (command.equals("bye")){
-                System.out.println("Bye. Hope to see you again soon!");
-                System.exit(0);
-            }
-            else if (command.equals("done")){
-                int index = input.nextInt();
-                System.out.println("Nice! I've marked this task as done:");
-                taskList[index-1].changeStatusIcon();
-                System.out.println("  "+taskList[index-1]);
-            }
+            try {
+                if (command.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int j = 0; j < i; j++) {
+                        System.out.println(j + 1 + "." + taskList[j]);
+                    }
+                } else if (command.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.exit(0);
+                } else if (command.equals("done")) {
+                    int index = input.nextInt();
+                    System.out.println("Nice! I've marked this task as done:");
+                    taskList[index - 1].changeStatusIcon();
+                    System.out.println("  " + taskList[index - 1]);
+                } else if (command.equals("todo")) {
+                    try {
+                        String content = input.nextLine();
+                        validateInput(content);
+                        taskList[i] = new Todo(content);
+                        outputTask(i);
 
-            else if (command.equals("todo")){
-                String content = input.nextLine();
-                taskList[i] = new Todo(content);
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  "+taskList[i].toString());
-                i++;
-                System.out.println("Now you have "+i+" tasks in the list.");
-            }else if (command.equals("deadline")){
-                String content = input.nextLine();
-                String d, t;
-                for (int k=0; k<content.length(); k++){
-                    if (content.charAt(k)=='/'){
-                        d = content.substring(0, k-1);
-                        t = content.substring(k+4);
-                        taskList[i] = new Deadline(d, t);
-                        break;
+                    } catch (DukeException e) {
+                        System.out.println("OOPS!!! The description of a todo cannot be empty.");
                     }
-                }
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  "+taskList[i]);
-                i++;
-                System.out.println("Now you have "+i+" tasks in the list.");
-            }else if (command.equals("event")){
-                String content = input.nextLine();
-                String d, t;
-                for (int k=0; k<content.length(); k++){
-                    if (content.charAt(k)=='/'){
-                        d = content.substring(0, k-1);
-                        t = content.substring(k+4);
-                        taskList[i] = new Event(d, t);
-                        break;
+
+                } else if (command.equals("deadline")) {
+                    String content = input.nextLine();
+                    String d, t;
+                    for (int k = 0; k < content.length(); k++) {
+                        if (content.charAt(k) == '/') {
+                            d = content.substring(0, k - 1);
+                            t = content.substring(k + 4);
+                            taskList[i] = new Deadline(d, t);
+                            break;
+                        }
                     }
+                    outputTask(i);
+                } else if (command.equals("event")) {
+                    String content = input.nextLine();
+                    String d, t;
+                    for (int k = 0; k < content.length(); k++) {
+                        if (content.charAt(k) == '/') {
+                            d = content.substring(0, k - 1);
+                            t = content.substring(k + 4);
+                            taskList[i] = new Event(d, t);
+                            break;
+                        }
+                    }
+                    outputTask(i);
+                    
+                } else {
+                    valid = false;
                 }
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  "+taskList[i]);
-                i++;
-                System.out.println("Now you have "+i+" tasks in the list.");
-            }else{
-                System.out.println("blah");
+
+                validateCommand(valid);
+            } catch (DukeException e) {
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
 
             System.out.print(line);
         }
     }
 
+    public static void outputTask(int i) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + taskList[i]);
+        i++;
+        System.out.println("Now you have " + i + " tasks in the list.");
+    }
+
+    public static void validateInput(String s) throws DukeException {
+        if (s.equals(""))
+            throw new DukeException("The description of this command cannot be empty.");
+    }
+
+    public static void validateCommand(boolean valid) throws DukeException {
+        if (valid == false)
+            throw new DukeException("The command is not valid");
+    }
 
 
 }
