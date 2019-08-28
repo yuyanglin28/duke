c3da8fb203d1c7a568cbc169e54354357e623629
yuyanglin28 <annielin9908@gmail.com>
2019-08-28 16:37:01 +0800

    Use Inheritance to support multiple task types

---
+++ src/main/java/Todo.java
@@ -0,0 +1,14 @@
+public class Todo extends Task{
+
+
+
+    public Todo(String description) {
+        super(description);
+
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
