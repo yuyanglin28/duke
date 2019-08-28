6e6608154917d7838371f9a4c47c6a4038166f26
yuyanglin28 <annielin9908@gmail.com>
2019-08-28 16:17:18 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ public class Duke {
     public static void main(String[] args) {
 
         String[] inputList = new String[100];
+        Task[] taskList = new Task[100];
         int i = 0;
 
 
@@ -15,11 +16,12 @@ public class Duke {
 
         Scanner input = new Scanner(System.in);
         while(true){
-            String command = input.nextLine();
+            String command = input.next();
             if (command.equals("list") ){
                 System.out.print(line);
+                System.out.println("Here are the tasks in your list:");
                 for ( int j = 0; j<i; j++){
-                    System.out.println(j+1+". "+inputList[j]);
+                    System.out.println(j+1+".["+taskList[j].getStatusIcon()+"]"+inputList[j]);
                 }
                 System.out.print(line);
             }else if (command.equals("blah")){
@@ -27,14 +29,24 @@ public class Duke {
             }else if (command.equals("bye")){
                 System.out.print(line+"Bye. Hope to see you again soon!\n"+line);
                 System.exit(0);
-            }else{
+            } else if (command.equals("done")){
+                int index = input.nextInt();
+                System.out.println(line+"Nice! I've marked this task as done:");
+                taskList[index-1].changeStatusIcon();
+                System.out.println("  ["+taskList[index-1].getStatusIcon()+"]  "+inputList[index-1]);
+                System.out.print(line);
+            }
+            else{
                 inputList[i] = command;
+                taskList[i] = new Task(command);
                 System.out.print(line+"added:"+command+"\n"+line);
                 i++;
             }
         }
-
-
-
     }
+
+
+
 }
+
+
