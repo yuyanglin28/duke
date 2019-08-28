e2134bdb33affd70b37cb26c9273cbf24df5f468
yuyanglin28 <annielin9908@gmail.com>
2019-08-28 14:39:36 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+
+        String line = "_______________________________________\n";
+        System.out.print(line);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?\n");
+        System.out.print(line);
+
+        Scanner input = new Scanner(System.in);
+        while(true){
+            String command = input.nextLine();
+            if (command == "list"){
+                System.out.print(line+"list\n"+line);
+            }else if (command == "blah"){
+                System.out.print(line+"blah\n"+line);
+            }else if (command == "bye"){
+                System.out.print(line+"Bye. Hope to see you again soon!\n"+line);
+                System.exit(0);
+            }else{
+                System.out.println("error");
+            }
+        }
+
+
+
     }
 }
